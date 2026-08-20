f3b79c6a5d5d78b9b8d3368e32ddb20a133ad125
campbellhatchard <campbell.hatchard@gmail.com>
2026-08-20 13:25:19 -0500

    Advance release version regression to v0.3.14

--- tests/test_delete_modal_routing_v0313.py
+++ tests/test_delete_modal_routing_v0313.py
@@ -56,5 +56,5 @@ def test_partial_invalid_draft_can_be_deleted_without_estimate_validation():
             assert db.get(Estimate, estimate_id) is None
 
 
-def test_release_version_is_v0313():
-    assert app.version == '0.3.13'
+def test_release_version_is_v0314():
+    assert app.version == '0.3.14'
