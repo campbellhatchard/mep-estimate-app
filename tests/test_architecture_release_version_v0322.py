0c82db2bc2b71fe26ec138271c1f59f947e206bf
campbellhatchard <campbell.hatchard@gmail.com>
2026-08-26 18:20:15 -0500

    Align bootstrap version guard to v0.3.25.1

--- tests/test_architecture_release_version_v0322.py
+++ tests/test_architecture_release_version_v0322.py
@@ -3,5 +3,5 @@ from app.run import app
 
 
 def test_bootstrap_release_version_is_active():
-    assert RELEASE_VERSION == "0.3.25.0"
+    assert RELEASE_VERSION == "0.3.25.1"
     assert app.version == RELEASE_VERSION
