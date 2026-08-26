1b049025bfe79a98b339b406f4f521f361d3d703
campbellhatchard <campbell.hatchard@gmail.com>
2026-08-26 18:11:24 -0500

    Bump hotfix version to 0.3.25.1

--- app/application_bootstrap.py
+++ app/application_bootstrap.py
@@ -45,7 +45,7 @@ from . import sow_layout_v3  # noqa: F401
 from . import sow_template_reconcile  # noqa: F401
 
 
-RELEASE_VERSION = "0.3.25.0"
+RELEASE_VERSION = "0.3.25.1"
 
 
 def _configure_roles() -> None:
